0e68d1604f3a3d6f230cf0c3bdf19d76ccb1185c
DevGuima <glecioheybel@hotmail.com>
2020-01-16 17:38:45 -0300

    Foi implementado as classes Pessoa, Diretor e Secretario ultilizando o conceito de herança da POO

---
+++ Curso-Java/src/cursojava/classes/Aluno.java
@@ -0,0 +1,197 @@
+package cursojava.classes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cusrsojava.constantes.StatusAluno;
+
+public class Aluno extends Pessoa {
+
+	
+	private String dataMatriculado;
+	private String nomeEscola;
+	private String serieMatriculado;
+	private List<Disciplina> disciplinas = new ArrayList<Disciplina>();
+
+	
+	/* construtor padrão do java */
+	public Aluno() {
+
+	}
+	/*construtor passando um parametro*/
+	public Aluno(String nomePadrao) {
+		super.nome = nomePadrao;
+
+	}
+  /*construtor com mais de um parametro*/
+	public Aluno(String nomePadrao, int idadePadrao) {
+		super.nome = nomePadrao;
+		super.idade = idadePadrao;
+
+	}
+	
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getRegistroGeral() {
+		return registroGeral;
+	}
+
+	public void setRegistroGeral(String registroGeral) {
+		this.registroGeral = registroGeral;
+	}  
+
+	public String getNumeroCPF() {
+		return numeroCPF;
+	}
+
+	public void setNumeroCPF(String numeroCPF) {
+		this.numeroCPF = numeroCPF;
+	}
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+
+	public String getNomePai() {
+		return nomePai;
+	}
+
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+
+	public String getDataMatriculado() {
+		return dataMatriculado;
+	}
+
+	public void setDataMatriculado(String dataMatriculado) {
+		this.dataMatriculado = dataMatriculado;
+	}
+
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+
+	public String getSerieMatriculado() {
+		return serieMatriculado;
+	}
+
+	public void setSerieMatriculado(String serieMatriculado) {
+		this.serieMatriculado = serieMatriculado;
+	}
+	
+	
+	public List<Disciplina> getDisciplinas() {
+		return disciplinas;
+	}
+
+	public void setDisciplinas(List<Disciplina> disciplinas) {
+		this.disciplinas = disciplinas;
+	}
+
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Aluno [nome=" + nome + ", idade=" + idade + ", dataNascimento=" + dataNascimento + ", registroGeral="
+				+ registroGeral + ", numeroCPF=" + numeroCPF + ", nomeMae=" + nomeMae + ", nomePai=" + nomePai
+				+ ", dataMatriculado=" + dataMatriculado + ", nomeEscola=" + nomeEscola + ", serieMatriculado="
+				+ serieMatriculado + ", disciplina=" + "]";
+	}
+
+	public double getMediaNota() {
+		
+		double somaNotas = 0.0;
+		
+		for (Disciplina disciplina : disciplinas) {
+			somaNotas += disciplina.getNota();
+			
+		}
+		
+		return somaNotas / disciplinas.size();
+	}
+
+	public boolean getAlunoAprovado() {
+
+		double media = this.getMediaNota();
+
+		if (media >= 70) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	public String getAlunoAprovado2() {
+
+		double media = this.getMediaNota();
+		if (media >= 50) {
+			if (media >= 70) {
+				return StatusAluno.APROVADO;
+			} else {
+				return StatusAluno.RECUPERACAO;
+			}
+		} else {
+			return StatusAluno.REPROVADO; 
+		}
+	}	
+	
+
+}
---
+++ Curso-Java/src/cursojava/classes/Secretario.java
@@ -0,0 +1,33 @@
+package cursojava.classes;
+
+public class Secretario extends Pessoa {
+
+	private String experiencia;
+	private String nivelCargo;
+	private String registro;
+
+	public String getExperiencia() {
+		return experiencia;
+	}
+
+	public void setExperiencia(String experiencia) {
+		this.experiencia = experiencia;
+	}
+
+	public String getNivelCargo() {
+		return nivelCargo;
+	}
+
+	public void setNivelCargo(String nivelCargo) {
+		this.nivelCargo = nivelCargo;
+	}
+
+	public String getRegistro() {
+		return registro;
+	}
+
+	public void setRegistro(String registro) {
+		this.registro = registro;
+	}
+
+}
