e9fb8b5d1a8a2e0e8251cbf063e957034c6ad076
DevGuima <glecioheybel@hotmail.com>
2020-05-03 00:41:14 -0300

    Brincando com Array

--- Curso-Java/src/cursojava/executavel/ArrayVetor.java
+++ Curso-Java/src/cursojava/executavel/ArrayVetor.java
@@ -1,11 +1,32 @@
 package cursojava.executavel;
 
+import javax.swing.JOptionPane;
+
 public class ArrayVetor {
 
 	public static void main(String[] args) {
 		
 		
-		double notas [];
+		String Posicoes = JOptionPane.showInputDialog("Quantas posiçoes o Array deve ter? ");
+		
+		
+		double[] notas = new double[Integer.parseInt(Posicoes)];
+		
+		
+		for (int pos = 0; pos < notas.length; pos++) {
+			String valor = JOptionPane.showInputDialog("Qual o valor da posição " + pos);
+			notas[pos] = Double.valueOf(valor);
+		}
+		
+	
+		for (int pos = 0; pos < notas.length; pos++) {
+			
+			System.out.println(" Nota " + (pos + 1) + " é " + notas[pos]);
+		}
+		
+		
+		
+		
 
 	}
 
